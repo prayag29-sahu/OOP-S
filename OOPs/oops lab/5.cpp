7b38aef491f41848614ee119fbb33bf52faceefa
Prayag Sahu <sahuprayag229@gmail.com>
2025-02-26 15:34:14 +0530

    Add files via upload

---
+++ OOPs/oops lab/5.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+using namespace std;
+
+class Demo
+{
+public:
+    static int objCount;
+    Demo()
+    {
+        objCount++;
+        cout << "Object no: " << objCount << " created" << endl;
+    }
+    ~Demo()
+    {
+        cout << "Object no: " << objCount << " Destroyed" << endl;
+        objCount--;
+    }
+};
+int Demo::objCount;
+
+int main()
+{
+    Demo d[3];
+}
